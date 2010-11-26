498443215109a820843e9fcf1ddccc5cf0ebe46e
Joshua Hawxwell <m@hawx.me>
2010-11-26 09:31:33 +0000

    Removed Test::Unit tests

--- test/suite.rb
+++
@@ -1,9 +0,0 @@
-require 'duvet'
-Duvet.start :filter => 'lib/ast_ast'
-
-# Test suite for Ast
-$: << File.join(File.dirname(__FILE__), '..')
-require File.join(File.dirname(__FILE__), 'helper')
-
-test_files = Dir.glob( File.join(File.dirname(__FILE__), "test_*.rb") )
-test_files.each {|f| require f }
