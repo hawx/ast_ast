e1651a0ed5577dbad7f1467137063462fcd88050
Joshua Hawxwell <m@hawx.me>
2010-11-26 09:13:26 +0000

    Moved Tokens to own file

--- lib/ast_ast.rb
+++ lib/ast_ast.rb
@@ -1,9 +1,10 @@
-$: << File.join(File.dirname(__FILE__), '..')
-__DIR__ = File.dirname(__FILE__)
+$: << File.dirname(__FILE__)
 
 require 'strscan'
 
-require File.join(__DIR__, 'ast_ast/ast')
-require File.join(__DIR__, 'ast_ast/tree')
-require File.join(__DIR__, 'ast_ast/tokeniser')
-require File.join(__DIR__, 'ast_ast/token')
+require 'ast_ast/ast'
+require 'ast_ast/tree'
+
+require 'ast_ast/tokeniser'
+require 'ast_ast/token'
+require 'ast_ast/tokens'
